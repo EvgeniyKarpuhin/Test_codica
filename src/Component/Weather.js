02b2026d8bbfd4a1717d519decea4433a9b68b58
EvgeniyKarpuhin <81818834+EvgeniyKarpuhin@users.noreply.github.com>
2022-02-06 20:52:19 +0200

    add

--- src/Component/Weather.js
+++ src/Component/Weather.js
@@ -1,53 +1,52 @@
-import React from 'react';
+import React from "react";
 
 class Weather extends React.Component {
-    state = {
-        weatherData: null
-    };
-    componentDidMount() {
-        const name = this.props.name;
-        const URL = 
-            "https://api.openweathermap.org/data/2.5/weather?q=" + name + 
-            "&lang=ru&units=metric&appid=a059085e330fce68f911321b65962676"
-        
-        fetch(URL)
-            .then(res => res.json())
-            .then(json => {
-            this.setState({ weatherData: json });
-        });
-    }
+  state = {
+    weatherData: null,
+  };
+  componentDidMount() {
+    const name = this.props.name;
+    const URL =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      name +
+      "&lang=ru&units=metric&appid=a059085e330fce68f911321b65962676";
 
-    render() {
-        const weatherData = this.state.weatherData;
-        if (!weatherData) return <div>Loading</div>;
-        const weather = weatherData.weather[0];
-        const iconUrl = "http://openweathermap.org/img/w/" + 
-        weather.icon + ".png";
-        return (
-            <div>
-                <h1>
-                     {weather.description}
-                    <img src={iconUrl} alt={weather.description} />
-                </h1>
-                <p>Температура: {weatherData.main.temp}°С</p>
-                <p>Скорость ветра: {weatherData.wind.speed} м/c</p>
-            </div>
-        );
-    }
+    fetch(URL)
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({ weatherData: json });
+      });
+  }
+
+  render() {
+    const weatherData = this.state.weatherData;
+    if (!weatherData) return <div>Loading</div>;
+    const weather = weatherData.weather[0];
+    const iconUrl = "http://openweathermap.org/img/w/" + weather.icon + ".png";
+    return (
+      <div className="weathers">
+          <img src={iconUrl} alt={weather.description} />
+        <h1>
+          {weather.description}
+        </h1>
+        <p>Температура: {weatherData.main.temp}°С</p>
+        <p>Скорость ветра: {weatherData.wind.speed} м/c</p>
+      </div>
+    );
+  }
 }
 
 // class Weather extends React.Component {
 //     // Props: day, key(index)
-  
-//     render() {    
+
+//     render() {
 //       const ms = this.props.city.dt * 1000;
 //       const weekdayName = new Date(ms).toLocaleString('ru', {city: 'long'});
-  
+
 //       const imgURL = "owf owf-"+ this.props.city.weather[0].id +" owf-5x icon-style"
-  
+
 //       // const farenheit = (parseInt(this.props.day.main.temp) - 273.15) * (9/5) + 32
-  
-  
+
 //       return (
 //         <div className="col-auto">
 //           <div className="card bg-light">
@@ -62,4 +61,31 @@ class Weather extends React.Component {
 //       )
 //     }
 //   }
+
+// class Weather extends React.Component {
+//     // Props: day, key(index)
+
+//     render() {
+//       const ms = this.props.day.dt * 1000;
+//       const weekdayName = new Date(ms).toLocaleString('ru', {weekday: 'long'});
+
+//       const imgURL = "owf owf-"+ this.props.day.weather[0].id +" owf-5x icon-style"
+
+//       // const farenheit = (parseInt(this.props.day.main.temp) - 273.15) * (9/5) + 32
+
+//       return (
+//         <div className="col-auto">
+//           <div className="card bg-light">
+//             <h3 className="card-title">{weekdayName}</h3>
+//             <i className={imgURL}></i>
+//             <h2>{Math.round(this.props.day.main.temp)} °C</h2>
+//             <div className="card-body">
+//               <button className="btn btn-dark btn-outline-light">{this.props.day.weather[0].description}</button>
+//             </div>
+//           </div>
+//         </div>
+//       )
+//     }
+//   }
+
 export default Weather;
--- src/Component/boxes.js
+++ src/Component/boxes.js
@@ -1,48 +1,39 @@
 import React from "react";
-import Weather from './Weather';
+import Weather from "./Weather";
 
 const capitalCity = ["Kiev", "Kharkiv", "Odessa", "Lviv"];
 
 class Boxes extends React.Component {
-    state = {
-      activePlace: 0,
-    };
+  state = {
+    activePlace: 0,
+  };
 
-    
-    render() {
-        const activePlaces = this.state.activePlace;
-        
-        return (
-          <div className="boxes">
-            
-            { capitalCity.map((city, index) => (
-              
-              <div className='countryBox'
-              style={
-                {width: '250px', 
-                height: '400px', 
-                border: '2px solid red', 
-                margin: '10px',
-                paddingTop: '10px'}
-              
-              }
-              key={index}
-              onClick={() =>
-              this.setState({activePlaces: index})
-    } 
-              >
-                
-           {city}     
-              <Weather
-              key={activePlaces}
-              name={capitalCity[activePlaces]} />
+  render() {
+    const activePlace = this.state.activePlace;
+
+    return (
+      <div className="boxes">
+        {capitalCity.map((city, index) => (
+          <div
+            className="countryBox"
+            style={{
+              width: "250px",
+              height: "400px",
+              border: "2px solid red",
+              margin: "10px",
+              paddingTop: "10px",
+            }}
+            key={index}
+            onClick={() => this.setState({ activePlace: index })}
+          >
+            {city}
+            <Weather key={activePlace} name={capitalCity[activePlace]} />
+          </div>
+        ))}
       </div>
-            ))} 
-              </div>
-        );
+    );
   }
 }
-
 
 // const weatherURL = "https://api.openweathermap.org/data/2.5/forecast?q=Kiev&lang=ru&units=metric&APPID=a059085e330fce68f911321b65962676";
 // const URL = {
@@ -66,6 +57,40 @@ class Boxes extends React.Component {
 
 //   formatCards = () => {
 //     return this.state.cities.map((city, index) => <Weather city={city} key={index}/>)
+//   }
+
+//   render() {
+//     return (
+//       <div className="">
+//       <h1 className="display-4 jumbotron">Прогноз погоды на 5 дней</h1>
+//       <h5 className="display-4 text-muted">Kiev</h5>
+//         <div className="row justify-content-center">
+
+//           {this.formatCards()}
+
+//         </div>
+//       </div>
+//     )
+//   }
+// }
+
+// const weatherURL = "https://api.openweathermap.org/data/2.5/forecast?q=Kiev&lang=ru&units=metric&APPID=a059085e330fce68f911321b65962676";
+// class Boxes extends React.Component {
+//   state = {
+//     days: []
+//   }
+
+//   componentDidMount = () => {
+//     fetch(weatherURL)
+//     .then(res => res.json())
+//     .then(data => {
+//       const dailyData = data.list.filter(reading => reading.dt_txt.includes("12:00:00"))
+//       this.setState({days: dailyData})
+//     })
+//   }
+
+//   formatCards = () => {
+//     return this.state.days.map((day, index) => <Weather day={day} key={index}/>)
 //   }
 
 //   render() {
